c3588aade5cf62c654853d1313a09654d2c6fc19
schlaepfAIR <99339120+schlaepfAIR@users.noreply.github.com>
2023-09-11 10:17:21 +0200

    Running 3 Tab App

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Tabs Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TabbedApp(),
+    );
+  }
+}
+
+class TabbedApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Tabs Demo'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'My Flights'),
+              Tab(text: 'Rest Time'),
+              Tab(text: 'Settings'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // My Flights Tab
+            Center(
+              child: Text('My Flights Tab Content'),
+            ),
+            // Rest Time Tab
+            Center(
+              child: Text('Rest Time Tab Content'),
+            ),
+            // Settings Tab
+            Center(
+              child: Text('Settings Tab Content'),
+            ),
+          ],
         ),
       ),
     );
